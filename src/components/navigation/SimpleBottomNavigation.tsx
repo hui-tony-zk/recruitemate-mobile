c6c81dfc2445ecee1bf7164565f4667a916896b2
Tony Hui <tony.hui@vid.io>
2023-03-26 06:55:58 -0700

    update navigation

--- src/components/navigation/SimpleBottomNavigation.tsx
+++ src/components/navigation/SimpleBottomNavigation.tsx
@@ -1,27 +1,36 @@
 import * as React from 'react';
+import Link from 'next/link';
+
 import Paper from '@mui/material/Paper';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import RestoreIcon from '@mui/icons-material/Restore';
+import HomeIcon from '@mui/icons-material/Home';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import CallIcon from '@mui/icons-material/Call';
 
 
 export default function SimpleBottomNavigation() {
   const [value, setValue] = React.useState(0);
 
   return (
-    <Paper elevation={3}>
+    <Paper elevation={3} sx={{width: '100%'}}>
       <BottomNavigation
         showLabels
         value={value}
         onChange={(event, newValue) => {
           setValue(newValue);
         }}
+        sx={{width: '100%'}}
       >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        <Link href="/">
+          <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+        </Link>
+        <Link href="/chat">
+          <BottomNavigationAction label="Call" icon={<CallIcon />} />
+        </Link>
+        <Link href="#">
+          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        </Link>
       </BottomNavigation>
     </Paper>
   );
